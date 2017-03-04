554cf760c741a415412593462e98d2a204ae0099
Mari Murotani <muromari5123@gmail.com>
2017-03-04 16:22:37 +0900

    Added for debugging

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Hello from '@/components/Hello'
 import Scene1 from '@/components/Scene1'
 import Scene2 from '@/components/Scene2'
+import Memo from '@/components/Memo'
 
 Vue.use(Router)
 
@@ -9,6 +11,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/scene1',
       name: 'Scene1',
       component: Scene1
     },
@@ -16,6 +23,11 @@ export default new Router({
       path: '/scene2',
       name: 'Scene2',
       component: Scene2
+    },
+    {
+      path: '/memo',
+      name: 'Memo',
+      component: Memo
     }
   ]
 })
