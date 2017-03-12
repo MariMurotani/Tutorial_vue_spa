87976cf29543c45c3a1480f4010332b7b799735c
Mari Murotani <muromari5123@gmail.com>
2017-03-12 18:52:36 +0900

    Fixed the problem of mutation.

--- src/models/PropertyStore.js
+++ src/models/PropertyStore.js
@@ -14,32 +14,34 @@ var PropertyStore = new Vuex.Store({
     increment (state, n) {
       state.count += n
     },
-    getUser (state) {
-      axios.get('http://127.0.0.1:4321/profile_wait')
-        .then(response => {
-          if (response.status === 200) {
-            state.property.nickname = response.data.nickname
-          }
-        })
+    getUser (state, response) {
+      state.property.nickname = response.data.nickname
     },
-    setUser (state) {
-      axios.post('http://127.0.0.1:4321/profile')
-        .then(response => {
-          if (response.status !== 200) {
-            state.property.nickname = 'eroor'
-          }
-        })
+    setUser (state, nickname) {
+      state.property.nickname = nickname
     }
   },
   actions: {
     getUser ({ commit }) {
-      commit('getUser')
+      axios.get('http://127.0.0.1:4321/profile_wait')
+        .then(response => {
+          if (response.status === 200) {
+            commit('getUser', response)
+          }
+        })
     },
     setUser ({ state, commit, rootState }) {
       if (state.property.nickname.length <= 3) {
         throw new Error('3文字以上入力してください')
       }
-      commit('setUser')
+      axios.post('http://127.0.0.1:4321/profile')
+        .then(response => {
+          if (response.status === 200 && response.data.status === 'ok') {
+            commit('setUser', state.property.nickname)
+          } else {
+            throw new Error('レスポンスエラー')
+          }
+        })
     }
   }
 })
